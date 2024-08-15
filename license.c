1d82488e62165019345ed3cabd35407f12f60f12
AdyaS2010 <adya.sastry@gmail.com>
2024-08-15 09:50:16 -0500

    Update and rename text-to-grade.c to license.c

---
+++ license.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    // Check for command line args
+    if (argc != 2)
+    {
+        printf("Usage: ./read infile\n");
+        return 1;
+    }
+
+    /*
+    // Create buffer to read into
+    char buffer[7];
+    */
+
+    // Create array to store plate numbers
+    char *plates[8];
+
+    FILE *infile = fopen(argv[1], "r");
+
+    int idx = 0;
+
+    while (!feof(infile)) // fread(buffer, 1, 7, infile) == 7)
+    {
+        // Allocate a new buffer for each plate number
+        char *buffer = malloc(8 * sizeof(char));
+}
